f7d25d724e543e88a1e01418e31ef577a9611c77
CainaNascimentoMelo <caina.melo@fatec.sp.gov.br>
2026-09-22 20:20:14 -0300

    remove(A6-32): Remove Akaer logo sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,9 @@
 import { FiFileText, FiLogOut } from "react-icons/fi";
-import logo from "../../assets/AkaerLogo.png";
 import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <aside className="sidebar">
-      <div className="sidebar-logo">
-        <div className="sidebar-logo-mark">
-          <img src={logo} alt="Akaer Logo" />
-        </div>
-      </div>
-
       <nav className="sidebar-menu">
         <button className="sidebar-item active">
           <FiFileText size={20} />
